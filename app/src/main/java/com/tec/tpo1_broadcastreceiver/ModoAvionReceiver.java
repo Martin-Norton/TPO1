c9127c0eaa233338afd09845ed7a45cdc8bd1010
Martin-Norton <117046765+Martin-Norton@users.noreply.github.com>
2025-08-25 18:41:43 -0300

    1ER COMMIT

---
+++ app/src/main/java/com/tec/tpo1_broadcastreceiver/ModoAvionReceiver.java
@@ -0,0 +1,27 @@
+package com.tec.tpo1_broadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.Toast;
+
+public class ModoAvionReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_AIRPLANE_MODE_CHANGED.equals(intent.getAction())) {
+            boolean modoAvionOn = intent.getBooleanExtra("state", false);
+
+            if (modoAvionOn) {
+                Toast.makeText(context, "Modo Avión ACTIVADO", Toast.LENGTH_LONG).show();
+                Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                callIntent.setData(Uri.parse("tel:2664553747"));
+                callIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(callIntent);
+            } else {
+                Toast.makeText(context, "Modo Avión DESACTIVADO", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
