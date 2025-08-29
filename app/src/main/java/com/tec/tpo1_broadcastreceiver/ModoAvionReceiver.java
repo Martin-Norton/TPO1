8dc08b76cfae10411bf6b384cf827082d5668993
Martin-Norton <117046765+Martin-Norton@users.noreply.github.com>
2025-08-29 16:12:59 -0300

    4

--- app/src/main/java/com/tec/tpo1_broadcastreceiver/ModoAvionReceiver.java
+++ app/src/main/java/com/tec/tpo1_broadcastreceiver/ModoAvionReceiver.java
@@ -10,16 +10,14 @@ public class ModoAvionReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-            boolean modoAvionOn = intent.getBooleanExtra("Estado", false);
+            boolean modoAvionCambiado = intent.getBooleanExtra("Estado", true);
 
-            if (modoAvionOn) {
-                Toast.makeText(context, "Modo Avión ACTIVADO", Toast.LENGTH_LONG).show();
+            if (modoAvionCambiado) {
+                Toast.makeText(context, "Modo Avión ha cambiado su estado", Toast.LENGTH_LONG).show();
                 Intent callIntent = new Intent(Intent.ACTION_DIAL);
                 callIntent.setData(Uri.parse("tel:2664553747"));
                 callIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(callIntent);
-            } else {
-                Toast.makeText(context, "Modo Avión DESACTIVADO", Toast.LENGTH_LONG).show();
             }
     }
 }
